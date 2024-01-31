02ec72356dd107b6bf00df2278977680f76d34c2
Trump-2 <akashidaiki2@gmail.com>
2024-01-31 21:28:05 +0800

    將修改和刪除按鈕改成用 html 語法的方式產生

--- back/admin.php
+++ back/admin.php
@@ -13,28 +13,30 @@
     $rows = $Admin->all();
     foreach ($rows as $row) {
     ?>
-    <tr>
-        <td class="pp ct"><?= $row['acc'] ?></td>
-        <!-- 把明碼的密碼改成用 * 代替；這裡不是 <input> 所以不能使用 input:password -->
-        <td class="pp ct"><?= str_repeat("*", strlen($row['pw'])) ?></td>
-        <td class="pp ct">
-            <?php
+        <tr>
+            <td class="pp ct"><?= $row['acc'] ?></td>
+            <!-- 把明碼的密碼改成用 * 代替；這裡不是 <input> 所以不能使用 input:password -->
+            <td class="pp ct"><?= str_repeat("*", strlen($row['pw'])) ?></td>
+            <td class="pp ct">
+                <?php
                 if ($row['acc'] == 'admin') {
                     echo "此帳號為最高權限";
                 } else {
-                    // 這裡使用 &#39; 因為前面已經有使用單引號了，但又需要單引號，所以用 &#39; 代替
-                    // 回去改成 html 的方式
-                    echo "<button onclick='location.href=&#39;?do=edit_admin&id={$row['id']}&#39;'>修改</button>";
-                    echo "<button onclick='del(&#39;admin&#39;,{$row['id']})'>刪除</button>";
+                ?>
+                    <!-- 這裡使用 &#39; 因為前面已經有使用單引號了，但又需要單引號，所以用 &#39; 代替
+                     改成用 html 語法的方式 -->
+                    <button onclick="location.href='?do=edit_admin&id=<?= $row['id'] ?>'">修改</button>
+                    <button onclick="del('admin',<?= $row['id'] ?>)">刪除</button>
+                <?php
                 }
                 ?>
-        </td>
-    </tr>
+            </td>
+        </tr>
 
     <?php
     }
     ?>
 </table>
-<div class="ct">
+<div class=" ct">
     <button type="button" onclick="location.href='./index.php'">返回</button>
 </div>
