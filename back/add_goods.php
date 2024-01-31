5a812994a2cbba539c57ecdfa3f9fd9e8a7010b5
YIHONG <akashidaiki2@gmail.com>
2024-01-31 16:34:47 +0800

    完成商品新增功能

--- back/add_goods.php
+++ back/add_goods.php
@@ -35,7 +35,7 @@
         </tr>
         <tr>
             <th class="tt ct">商品圖片</th>
-            <td class="pp"><input type="text" name="img" value=""></td>
+            <td class="pp"><input type="file" name="img" value=""></td>
         </tr>
         <tr>
             <th class="tt ct">商品介紹</th>
@@ -62,7 +62,7 @@
             switch (type) {
                 case "big":
                     $("#big").html(types)
-                    getType('mid', $("#big").val())
+                    getTypes('mid', $("#big").val())
                     break;
                 case "mid":
                     $("#mid").html(types)
