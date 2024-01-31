6bfea458d3e570d52921301eaba0028284e4ac8a
YIHONG <akashidaiki2@gmail.com>
2024-01-31 11:24:25 +0800

    添加註解

--- api/reg.php
+++ api/reg.php
@@ -7,6 +7,7 @@ if (!isset($_POST['id'])) { // 沒有 id 代表要註冊
     $_POST['regdate'] = date("Y-m-d"); // 因為表單送過來的欄位沒有 regdate，所以手動指定
 }
 
+// 新增、修改都要存回資料庫
 $Mem->save($_POST);
 
 if (isset($_POST['id'])) { // 有 id 代表要編輯會員資料
