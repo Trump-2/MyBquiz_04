f01e1098435736762ad8404c843dbc2818f64d17
YIHONG <akashidaiki2@gmail.com>
2024-02-01 13:30:48 +0800

    修改 $_POST[ 'img' ] 為上傳的檔案的名稱

--- api/save_goods.php
+++ api/save_goods.php
@@ -3,7 +3,7 @@
 include "db.php";
 
 if (!empty($_FILES['img']['tmp_name'])) {
-    $_POST['img'] = $_FILES['img']['tmp_name'];
+    $_POST['img'] = $_FILES['img']['name'];
     move_uploaded_file($_FILES['img']['tmp_name'], "../img/{$_FILES['img']['name']}");
 }
 $_POST['no'] = rand(100000, 999999);
