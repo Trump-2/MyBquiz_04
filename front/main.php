adab463b34ae4788d0c161888096f2ebd024b8e8
YIHONG <akashidaiki2@gmail.com>
2024-02-16 14:27:17 +0800

    建立購物車登入登出狀態判斷

--- front/main.php
+++ front/main.php
@@ -23,63 +23,64 @@ if ($type == 0) {
 <h2><?= $nav ?></h2>
 
 <style>
-    .item {
-        width: 80%;
-        height: 160px;
-        background: #f4c591;
-        margin: 5px auto;
-        display: flex;
-    }
+.item {
+    width: 80%;
+    height: 160px;
+    background: #f4c591;
+    margin: 5px auto;
+    display: flex;
+}
 
-    .item .img {
-        width: 33%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border: 1px solid #999;
-    }
+.item .img {
+    width: 33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #999;
+}
 
-    .item .info {
-        width: 67%;
-        display: flex;
-        flex-direction: column;
-    }
+.item .info {
+    width: 67%;
+    display: flex;
+    flex-direction: column;
+}
 
-    .info div {
-        border: 1px solid #999;
-        border-left: 0;
-        border-top: 0;
-        flex-grow: 1;
-    }
+.info div {
+    border: 1px solid #999;
+    border-left: 0;
+    border-top: 0;
+    flex-grow: 1;
+}
 
-    /* 也可以寫成行內 style，就不用這個選擇器 */
-    .info div:nth-child(1) {
-        border-top: 1px solid #999;
-    }
+/* 也可以寫成行內 style，就不用這個選擇器 */
+.info div:nth-child(1) {
+    border-top: 1px solid #999;
+}
 </style>
 
 <?php
 foreach ($goods as $good) {
 
 ?>
-    <div class="item">
-        <div class="img">
-            <a href="?do=detail&id=<?= $good['id'] ?>">
+<div class="item">
+    <div class="img">
+        <a href="?do=detail&id=<?= $good['id'] ?>">
 
-                <img src="./img/<?= $good['img'] ?>" style="width:80%;height:110px">
-            </a>
-        </div>
-
-        <div class="info">
-            <div class="ct tt"><?= $good['name'] ?></div>
-            <div>
-                價錢 : <?= $good['price'] ?>
-                <img src="./icon/0402.jpg" style="float:right">
-            </div>
-            <div>規格 : <?= $good['spec'] ?></div>
-            <div>簡介 : <?= mb_substr($good['intro'], 0, 25) ?>...</div>
-        </div>
+            <img src="./img/<?= $good['img'] ?>" style="width:80%;height:110px">
+        </a>
     </div>
+
+    <div class="info">
+        <div class="ct tt"><?= $good['name'] ?></div>
+        <div>
+            價錢 : <?= $good['price'] ?>
+            <img src="./icon/0402.jpg" style="float:right"
+                onclick="location.href='?do=buycart&id=<?= $good['id'] ?>&qt=1'">
+        </div>
+        <div>規格 : <?= $good['spec'] ?></div>
+        <div>簡介 : <?= mb_substr($good['intro'], 0, 25) ?>...</div>
+    </div>
+</div>
 <?php
 }
 
