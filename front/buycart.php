d58a5001e15003263145fafb023659a3eed12072
YIHONG <akashidaiki2@gmail.com>
2024-02-16 15:35:18 +0800

    完成點選我要購買,在購物車中顯示商品

--- front/buycart.php
+++ front/buycart.php
@@ -11,7 +11,42 @@ echo "<h2 class='ct'>{$_SESSION['mem']}的購物車</h2>";
 
 if (!isset($_SESSION['cart'])) {
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
-} else {
-
-    dd($_SESSION['cart']);
 }
+// else {
+
+//     dd($_SESSION['cart']);
+// }
+?>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+    <?php
+    foreach ($_SESSION['cart'] as $id => $qt) {
+        $goods = $Goods->find($id)
+    ?>
+
+        <tr class="pp ct">
+            <td><?= $goods['no'] ?></td>
+            <td><?= $goods['name'] ?></td>
+            <td><?= $qt ?></td>
+            <td><?= $goods['stock'] ?></td>
+            <td><?= $goods['price'] ?></td>
+            <td><?= $goods['price'] * $qt ?></td>
+            <td><img src="./icon/0415.jpg" alt=""></td>
+        </tr>
+    <?php
+
+    }
+    ?>
+</table>
+<div class="ct">
+    <img src="./icon/0411.jpg" alt="">
+    <img src="./icon/0412.jpg" alt="">
+</div>
