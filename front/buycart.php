86d8792af07d70c50a8ffdaa6eb27c60a1915af1
Trump-2 <akashidaiki2@gmail.com>
2024-02-18 16:27:28 +0800

    添加註解

--- front/buycart.php
+++ front/buycart.php
@@ -1,4 +1,5 @@
 <?php
+// 這段放在這裡的好處是，沒有登入前就可以將商品放入購物車中了
 if (isset($_GET['id'])) {
     $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
 }
@@ -9,6 +10,7 @@ if (!isset($_SESSION['mem'])) {
 
 echo "<h2 class='ct'>{$_SESSION['mem']}的購物車</h2>";
 
+// 用 session 來判斷有沒有購買商品
 if (empty($_SESSION['cart'])) {
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
 }
@@ -32,15 +34,15 @@ if (empty($_SESSION['cart'])) {
         $goods = $Goods->find($id)
     ?>
 
-    <tr class="pp ct">
-        <td><?= $goods['no'] ?></td>
-        <td><?= $goods['name'] ?></td>
-        <td><?= $qt ?></td>
-        <td><?= $goods['stock'] ?></td>
-        <td><?= $goods['price'] ?></td>
-        <td><?= $goods['price'] * $qt ?></td>
-        <td><img src="./icon/0415.jpg" alt="" onclick="delCart(<?= $id ?>)"></td>
-    </tr>
+        <tr class="pp ct">
+            <td><?= $goods['no'] ?></td>
+            <td><?= $goods['name'] ?></td>
+            <td><?= $qt ?></td>
+            <td><?= $goods['stock'] ?></td>
+            <td><?= $goods['price'] ?></td>
+            <td><?= $goods['price'] * $qt ?></td>
+            <td><img src="./icon/0415.jpg" alt="" onclick="delCart(<?= $id ?>)"></td>
+        </tr>
     <?php
 
     }
@@ -52,13 +54,13 @@ if (empty($_SESSION['cart'])) {
 </div>
 
 <script>
-function delCart(id) {
-    $.post("./api/del_cart.php", {
-        id
-    }, () => {
-        // .reload():代表不修改原網址的情況下對該畫面重新請求
-        // location.reload()
-        location.href = "?do=buycart"
-    })
-}
+    function delCart(id) {
+        $.post("./api/del_cart.php", {
+            id
+        }, () => {
+            // .reload():代表不修改原網址的情況下對該畫面重新請求
+            // location.reload()
+            location.href = "?do=buycart"
+        })
+    }
 </script>
