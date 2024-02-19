0255a04da48e4b4b9e924ec75eeb732b7d4d401a
YIHONG <akashidaiki2@gmail.com>
2024-02-19 10:27:47 +0800

    解決結帳完之後直接點選購物車連結的錯誤訊息

--- front/buycart.php
+++ front/buycart.php
@@ -13,54 +13,56 @@ echo "<h2 class='ct'>{$_SESSION['mem']}的購物車</h2>";
 // 用 session 來判斷有沒有購買商品，這裡用 empty 而不是 !isset()
 if (empty($_SESSION['cart'])) {
     echo "<h2 class='ct'>購物車中尚無商品</h2>";
-}
-// else {
+} else {
 
-//     dd($_SESSION['cart']);
-// }
+
+
 ?>
-<table class="all">
-    <tr class="tt ct">
-        <td>編號</td>
-        <td>商品名稱</td>
-        <td>數量</td>
-        <td>庫存量</td>
-        <td>單價</td>
-        <td>小計</td>
-        <td>刪除</td>
-    </tr>
-    <?php
-    foreach ($_SESSION['cart'] as $id => $qt) {
-        $goods = $Goods->find($id)
-    ?>
-
-        <tr class="pp ct">
-            <td><?= $goods['no'] ?></td>
-            <td><?= $goods['name'] ?></td>
-            <td><?= $qt ?></td>
-            <td><?= $goods['stock'] ?></td>
-            <td><?= $goods['price'] ?></td>
-            <td><?= $goods['price'] * $qt ?></td>
-            <td><img src="./icon/0415.jpg" alt="" onclick="delCart(<?= $id ?>)"></td>
+    <table class="all">
+        <tr class="tt ct">
+            <td>編號</td>
+            <td>商品名稱</td>
+            <td>數量</td>
+            <td>庫存量</td>
+            <td>單價</td>
+            <td>小計</td>
+            <td>刪除</td>
         </tr>
-    <?php
+        <?php
+        foreach ($_SESSION['cart'] as $id => $qt) {
+            $goods = $Goods->find($id)
+        ?>
 
-    }
-    ?>
-</table>
-<div class="ct">
-    <img src="./icon/0411.jpg" alt="" onclick="location.href='index.php'">
-    <img src="./icon/0412.jpg" alt="" onclick="location.href='?do=checkout'">
-</div>
+            <tr class="pp ct">
+                <td><?= $goods['no'] ?></td>
+                <td><?= $goods['name'] ?></td>
+                <td><?= $qt ?></td>
+                <td><?= $goods['stock'] ?></td>
+                <td><?= $goods['price'] ?></td>
+                <td><?= $goods['price'] * $qt ?></td>
+                <td><img src="./icon/0415.jpg" alt="" onclick="delCart(<?= $id ?>)"></td>
+            </tr>
+        <?php
 
-<script>
-    function delCart(id) {
-        $.post("./api/del_cart.php", {
-            id
-        }, () => {
-            // .reload():代表不修改原網址的情況下對該畫面重新請求
-            // location.reload()
-            location.href = "?do=buycart"
-        })
-    }
-</script>
+        }
+        ?>
+    </table>
+    <div class="ct">
+        <img src="./icon/0411.jpg" alt="" onclick="location.href='index.php'">
+        <img src="./icon/0412.jpg" alt="" onclick="location.href='?do=checkout'">
+    </div>
+
+    <script>
+        function delCart(id) {
+            $.post("./api/del_cart.php", {
+                id
+            }, () => {
+                // .reload():代表不修改原網址的情況下對該畫面重新請求
+                // location.reload()
+                location.href = "?do=buycart"
+            })
+        }
+    </script>
+<?php
+}
+?>
